65bcb9f159a6dd745a83528a23ac80304ee158fc
lukivan8 <lukivan888@gmail.com>
2023-03-11 21:41:29 +0600

    Remade enums using numbers

--- src/core/bill/enums/create-bill.enum.ts
+++ src/core/bill/enums/create-bill.enum.ts
@@ -1,20 +1,20 @@
 export enum TRANSACTION_TYPE_ENUM {
-  INTERNET="internet",
-  COMMUNAL="commun",
-  EDUCATION="education"
+  INTERNET=1,
+  COMMUNAL=2,
+  EDUCATION=3
 }
 export type TRANSCTION_TYPE = `${TRANSACTION_TYPE_ENUM}`
 
 export enum PROVIDER_ENUM {
-  FREGAT="fregat-tv",
-  KIEVSTAR="kievstar",
-  UKRTELEKOM="ukrtelekom",
-  COMPROV_1="compov1",
-  COMPROV_2="compov2",
-  COMPROV_3="compov3",
-  EDUPROV_1="eduprov1",
-  EDUPROV_2="eduprov2",
-  EDUPROV_3="eduprov3",
+  FREGAT=1,
+  KIEVSTAR=2,
+  UKRTELEKOM=3,
+  COMPROV_1=4,
+  COMPROV_2=5,
+  COMPROV_3=6,
+  EDUPROV_1=7,
+  EDUPROV_2=8,
+  EDUPROV_3=9,
 }
 
 
